68e4aa7c46caba265bb251447a8f8d7b77f6652b
devanandhaaroop <devanandhaaroop9@gmail.com>
2026-09-04 02:16:15 +0530

    prepare for backend deployment

--- backend/server.js
+++ backend/server.js
@@ -149,7 +149,7 @@ app.get("/api/prediction/:category", (req, res) => {
 // START SERVER
 // ==========================================
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(
